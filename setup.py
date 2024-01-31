354817349722bc4870c995dc1f8f78d1b103b905
Vivek Dagar <vivekdagar2023@gmail.com>
2024-01-31 22:02:38 +0530

    chore: add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyquantify',
-    version='1.0.1',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=[
-        'click',
-        'nltk',
-        'tabulate',
+        'click~=8.1.7',
+        'nltk~=3.8.1',
+        'tabulate~=0.9.0',
+        'requests~=2.31.0',
+        'beautifulsoup4~=4.12.2',
+        'bert-extractive-summarizer~=0.10.1',
+        'pandas~=2.1.4',
+        'joblib~=1.2.0',
+        'scikit-learn~=1.3.0',
+        'matplotlib~=3.7.2',
+        'seaborn~=0.12.2',
+        'spacy~=3.7.2',
+        'wordcloud~=1.9.2',
+        'textblob~=0.15.3',
+        'setuptools~=68.0.0',
     ],
     entry_points={
         'console_scripts': [
